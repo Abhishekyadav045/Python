881c88c7baac8ef21b63134e0af797ed1103c65f
Abhishek Yadav <ay407473@gmail.com>
2026-02-06 09:19:06 +0530

    Add files via upload

---
+++ 1First.py
@@ -0,0 +1,31 @@
+# print("hello world")
+
+# a=int(input("Enter first number "))
+# b=int(input("Enter second number "))
+# print("Sum",a+b)
+# print("Difference", a-b)
+# print("Multiply",a*b)
+
+# x=15
+# y=4
+# print(x//y)
+
+# u=int(input("Enter first number "))
+# v=int(input("Enter second number "))
+# w=int(input("Enter third number "))
+# x=int(input("Enter fourth number "))
+# y=int(input("Enter five number "))
+# Avg=(u+v+w+x+y) / 5
+# print("Average of number", Avg)
+
+# n=int(input("Enter the number "))
+# square=n**2
+# cube=n**3
+# print("Sqr", square)
+# print("cub",cube)
+
+sec= int(input("Enter Seconds "))
+min=(sec//60 )
+second= (sec%60)
+print(min ,"Min",second,"Sec")
+
---
+++ 3loops.py
@@ -0,0 +1,56 @@
+# for i in range(5):
+#     print(i)
+
+# count=0
+# while count<5:
+#     print(count)
+#     count+=1
+
+
+# for i in range(1,11):
+#     print(i)
+
+
+# sum=0
+# i=1
+# while i<11:
+#     sum+=i
+#     i+=1
+# print(sum)
+
+# num=int(input("Enter the number: "))
+# for i in range(1,11):
+#     print(num*i)
+
+# num=int(input("Enter the number: "))
+# fact=1
+# i=1
+# while i<=num:
+#     fact*=i
+#     i+=1
+# print(fact)
+
+# num=int(input("Enter the number: "))
+# rev=0
+# while num>0:
+#     digit =num%10
+#     rev = rev*10 + digit
+#     num=num//10
+# print(rev)
+
+# num=int(input("Enter the number: "))
+# a=0
+# b=1
+# i=1
+
+# for i in range(num):
+#     c=a+b
+#     print(c)
+#     a=b
+#     b=c
+
+
+for i in range(6):
+    for j in range(i):
+        print("*",end="")
+    print()
