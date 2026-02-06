f3e25f792f41755d73a6dcc243fe7cafd4f2964c
Abhishek Yadav <ay407473@gmail.com>
2026-02-06 21:26:17 +0530

    Add files via upload

---
+++ 4func.py
@@ -0,0 +1,100 @@
+# def name():
+#     print("Hello , World")
+
+# name()
+
+
+# def hello(name):
+#     print("Hello", name)
+# hello("Abhi")
+
+
+# def sum(a,b):
+#     print(a+b)
+# sum(4,5)
+
+
+# def squ(x):
+#     return x**2
+# square=squ(35)
+# print(square)
+
+
+# x=int(input("enter the number: "))
+# def num(x):
+#     if x%2==0:
+#         print("Number is even ")
+#     else:
+#         print("number is odd ")
+# num(x)
+
+
+# def num(x ,y):
+#     if x>y:
+#         print("Max x")
+#     else:
+#         print("Max y")
+# num(9,8)
+
+
+# x=int(input("Enter the first number:"))
+# y=int(input("Enter the second number:"))
+# def num(x ,y):
+#     if x>y:
+#         print(x,"number is greater")
+#     else:
+#         print(y,"Number is greater")
+# num(x,y)
+
+
+# def num_avg(number):
+#     total=0
+#     for num in number:
+#         total+=num
+#     avg=total/5
+#     return avg
+# average=num_avg([2,9,5,5,8])
+# print(average)
+
+
+# def co_vowels(s):
+#     vowels = "aeiouAEIOU"
+#     count = 0
+
+#     for char in s:
+#         if char in vowels:
+#             count += 1
+
+#     return count
+
+# word= "Uttar pradesh"
+# print(co_vowels(word))
+
+
+
+# def fact(y):
+#     i=1
+#     facto= 1
+#     while i<=y:
+#       facto*=i
+#       i+=1
+#     return facto
+# result= fact(6) 
+# print(result)
+
+
+
+
+# def is_palindrome(text):
+#    text=text.lower()
+#    rev=""
+
+#    for char in text:
+#       rev= char+ rev
+#    if text == rev:
+#       print("number is palindrom")
+#    else:
+#       print("number is not palindrome")
+# is_palindrome("abakaba")
+
+
